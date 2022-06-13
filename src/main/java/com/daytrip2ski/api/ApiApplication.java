c866a62af8904010fc359daf9aec8832c2039bac
Martin <martin.angermair@angertech.com>
2022-06-13 07:49:10 +0200

    Add Swagger config

--- src/main/java/com/daytrip2ski/api/ApiApplication.java
+++ src/main/java/com/daytrip2ski/api/ApiApplication.java
@@ -2,7 +2,17 @@ package com.daytrip2ski.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -10,5 +20,28 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/v1/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.daytrip2ski.api"))
+				.build()
+				.apiInfo(apiDetails());
+	}
+
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"daytrip2Ski API",
+				"Daytrip2ski API for Planning your Skitrip",
+				"1.0",
+				"For use ask daytrip2ski development team",
+				new springfox.documentation.service.Contact("A-Team", "http://mci.edu", "sm1678@mci4me.at"),
+				"API License",
+				"http://mci.edu",
+				Collections.emptyList()
+		);
 	}
 }
